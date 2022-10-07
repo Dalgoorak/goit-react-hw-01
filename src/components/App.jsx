53a7208ca2943c6a6234af3f40638fa06a259743
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-10-07 22:01:17 +0300

    create task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
 import { UserProfile } from './SocialProfile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import Section from './Sections/Section';
 import user from './data/user.json';
+import data from './data/data.json';
 
 export const App = () => {
   return (
     <div>
-      <UserProfile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      <Section title={'Task - 1'}>
+        <UserProfile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section title={'Task - 2'}>
+        <Statistics title="Upload stats" stats={data} />
+      </Section>
+
+      <Section>
+        <Statistics stats={data} />
+      </Section>
     </div>
   );
 };
