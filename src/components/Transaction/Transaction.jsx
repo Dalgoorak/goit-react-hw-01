54f68cbcbfaab9f6888530ea944be98caee0fa66
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-10-09 13:50:18 +0300

    create task 3 and task 4

---
+++ src/components/Transaction/Transaction.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './Transaltion.module.css';
+
+export const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className={css.transaction_history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+    })
+  ).isRequired,
+};
